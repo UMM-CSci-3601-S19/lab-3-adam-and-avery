12185a2d8d56fab016608df94bec430b44c48372
Adam Schug <schug047@morris.umn.edu>
2019-02-21 14:28:16 -0600

    Add entries for TodoListComponent and TodoListService

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,8 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+import {TodoListComponent} from "./todos/todo-list.component";
+import {TodoListService} from "./todos/todo-list.service";
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
@@ -27,10 +29,12 @@ import {UserComponent} from './users/user.component';
     HomeComponent,
     UserListComponent,
     UserComponent,
+    TodoListComponent,
 
   ],
   providers: [
     UserListService,
+    TodoListService,
     {provide: APP_BASE_HREF, useValue: '/'},
     {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
   ],
